87b421fc582edb3b3060dea103593adcab7157a8
Changzhou Li <45784976+Changzhouli3309@users.noreply.github.com>
2021-04-16 09:01:36 +0200

    add alert
    add alert when fail to create or update

--- src/App.js
+++ src/App.js
@@ -6,109 +6,126 @@ import Printa from './Printa';
 function App() {
   
 
- const [pokemons, setPokemons] = useState([])
- const [omegaHook, callOmegaHook] = useState(9999)
-
+  const [pokemons, setPokemons] = useState([])
+  const [omegaHook, setOmegaHook] = useState(true)
  
-  useEffect(() => {
-    async function fetchPokemons(){
-      const resp = await fetch('http://localhost:8080/api/students')
-      const pokemon = await resp.json()
-      const pokemon50 = [...pokemon];      
-      setPokemons(pokemon50);      
-      console.log(pokemon50);
-    }
-    fetchPokemons();        
-  },[omegaHook]);
-
+  
+   useEffect(() => {
+     async function fetchPokemons(){
+       const resp = await fetch('http://localhost:8080/api/students')
+       
+       if (resp.status===204) {
+         setPokemons([]);
+         
+       } else {
+         const pokemon = await resp.json()
+         const pokemon50 = [...pokemon];
+         setPokemons(pokemon50);
+       }
+       console.log(pokemons);
+     }
+     fetchPokemons();        
+   },[omegaHook]);
+ 
+   function callOmegaHook(){
+     setOmegaHook(!omegaHook)
+   }
+  
+   function removePokemon(x){  
+    /* let omega = Math.floor(Math.random()* 10000000000); */
+     async function fetchDel(){
+       const resp = await fetch(`http://localhost:8080/api/student/${pokemons[x].student_id}`, {
+         method: 'DELETE',
+         headers: { 'Content-Type': 'application/json' }
+       })
+       if (resp.ok){
+           callOmegaHook();
+       }
+     }
+     fetchDel();    
+   }
  
  
-  function removePokemon(x){  
-   let omega = Math.floor(Math.random()* 10000000000);
-    async function fetchDel(){
-      const resp = await fetch(`http://localhost:8080/api/student/${pokemons[x].student_id}`, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' }
-      })
-      if (resp.ok){
-          callOmegaHook(omega);
-      }
-    }
-    fetchDel();    
-  }
-
-
-
-  function addUser(Xfirstname, Xlastname, Xage, Ypresent){
-  let omega = Math.floor(Math.random()* 10000000000);
-    async function fetchAdd(){
-      const resp = await fetch('http://localhost:8080/api/student', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          name: Xfirstname,
-          last_name: Xlastname,
-          age: Xage,          
-          present: Ypresent
-        })          
-      })                 
-    }
-    fetchAdd(); 
-    callOmegaHook(omega);      
-  }
-
-
-
-  function checkBoxChanged(i, h){
-   
-    let omega = Math.floor(Math.random()* 10000000000);
-    async function fetchCheck(){
-      const resp = await fetch(`http://localhost:8080/api/student/${pokemons[i].student_id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          name: pokemons[i].name,
-          last_name: pokemons[i].last_name,
-          age: pokemons[i].age,          
-          present: h
-        })          
-      })                 
-    }
-    fetchCheck(); 
-    callOmegaHook(omega);     
-  }
-
-  function updateUser(i, Xfirstname, Xlastname, Xage, Ypresent){    
-    let omega = Math.floor(Math.random()* 10000000000);
-    async function fetchUpdate(){
-      const resp = await fetch(`http://localhost:8080/api/student/${pokemons[i].student_id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          name: Xfirstname,
-          last_name: Xlastname,
-          age: Xage,          
-          present: Ypresent
-        })          
-      })                 
-    }
-    fetchUpdate(); 
-    callOmegaHook(omega);   
-  }
-
-  return (
-        <div>
-        <Printa 
-        poke={pokemons}
-        removePokemon = {removePokemon}
-        addUser = {addUser}
-        checkBoxChanged = {checkBoxChanged}
-        updateUser = {updateUser}
-        />        
-      </div>  
-  )
  
-}
-
-export default App;
-
+   function addUser(Xfirstname, Xlastname, Xage, Ypresent){
+   /* let omega = Math.floor(Math.random()* 10000000000); */
+     async function fetchAdd(){
+       const resp = await fetch('http://localhost:8080/api/student', {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({
+           name: Xfirstname,
+           last_name: Xlastname,
+           age: Xage,          
+           present: Ypresent
+         })          
+       })
+       if(resp.status===201){
+         callOmegaHook();
+       }else{
+         alert("Fail to add, check your input")
+       }                
+     }
+     fetchAdd(); 
+           
+   }
+ 
+ 
+ 
+   function checkBoxChanged(i, h){
+    
+     /* let omega = Math.floor(Math.random()* 10000000000); */
+     async function fetchCheck(){
+       const resp = await fetch(`http://localhost:8080/api/student/${pokemons[i].student_id}`, {
+         method: 'PUT',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({
+           name: pokemons[i].name,
+           last_name: pokemons[i].last_name,
+           age: pokemons[i].age,          
+           present: h
+         })          
+       })                 
+     }
+     fetchCheck(); 
+     callOmegaHook();     
+   }
+ 
+   function updateUser(i, Xfirstname, Xlastname, Xage, Ypresent){    
+ /*     let omega = Math.floor(Math.random()* 10000000000); */
+     async function fetchUpdate(){
+       const resp = await fetch(`http://localhost:8080/api/student/${pokemons[i].student_id}`, {
+         method: 'PUT',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({
+           name: Xfirstname,
+           last_name: Xlastname,
+           age: Xage,          
+           present: Ypresent
+         })          
+       })
+       if(resp.status===200){
+         callOmegaHook();
+       }else{
+         alert("Fail to update, check your input")
+       }                    
+     }
+     fetchUpdate();  
+   }
+ 
+   return (
+         <div>
+         <Printa 
+         poke={pokemons}
+         removePokemon = {removePokemon}
+         addUser = {addUser}
+         checkBoxChanged = {checkBoxChanged}
+         updateUser = {updateUser}
+         />        
+       </div>  
+   )
+  
+ }
+ 
+ export default App;
+  
--- src/Printa.js
+++ src/Printa.js
@@ -28,13 +28,13 @@ function PrintaPoks(props){
           <p>{p.last_name} </p>
           &nbsp;
           &nbsp;         
-          <label className="check" for={p.index}><input type="checkbox" checked={p.present} onChange={e => {props.checkBoxChanged(index, e.target.checked)}}></input><p className="box"></p></label>      
+          <label className="check" htmlFor={p.index}><input type="checkbox" checked={p.present} onChange={e => {props.checkBoxChanged(index, e.target.checked)}}></input><p className="box"></p></label>      
           &nbsp;
           &nbsp;
           &nbsp;
-          <button className="square shape" onClick={e => {setSida(true); XsetPokemons(p.name); XsetLastname(p.last_name); i = index; XsetNumber(p.age); XsetPresent(p.present)}}> <span class="text2">edit</span> </button>
+          <button className="square shape" onClick={e => {setSida(true); XsetPokemons(p.name); XsetLastname(p.last_name); i = index; XsetNumber(p.age); XsetPresent(p.present)}}> <span className="text2">edit</span> </button>
           &nbsp;
-          <button className="square shape" onClick={e => {props.removePokemon(index)}}><span class="text2">del</span></button>                   
+          <button className="square shape" onClick={e => {props.removePokemon(index)}}><span className="text2">del</span></button>                   
           </div>)}
           <div></div>    
           <div></div>
@@ -44,7 +44,7 @@ function PrintaPoks(props){
           <p>Lastname: <input type="text" value={Xlastname} onChange={e =>{XsetLastname(e.target.value)}}></input></p>
           <p>Age: <input type="number" value={Xnumber} onChange={e =>{XsetNumber(e.target.value)}}/></p>          
           <div className="omega"><label className="check"><input type="checkbox" checked={Xpresent} onChange={e =>{XsetPresent(e.target.checked)}}/><p className="box"></p></label></div>
-          <button className="square shape" onClick={e => {props.addUser(Xpokemons, Xlastname, Xnumber, Xpresent); XsetPokemons(""); XsetLastname(""); XsetNumber(""); XsetPresent(true)}}> <span class="text2">add</span> </button>            
+          <button className="square shape" onClick={e => {props.addUser(Xpokemons, Xlastname, Xnumber, Xpresent); XsetPokemons(""); XsetLastname(""); XsetNumber(""); XsetPresent(true)}}> <span className="text2">add</span> </button>            
           </div>
     </div>
   )
@@ -59,8 +59,8 @@ return <div>
    <p>Lastname: <input type="text" value={Xlastname} onChange={e =>{XsetLastname(e.target.value)}}></input></p>
    <p>Age: <input type="number" value={Xnumber} onChange={e =>{XsetNumber(e.target.value)}}/></p>
    <div className="omega"><label className="check"><input type="checkbox" checked={Xpresent} onChange={e =>{XsetPresent(e.target.checked)}}/><p className="box"></p></label></div>
-   <button className="square shape" onClick={e => {setSida(false); props.updateUser(i, Xpokemons, Xlastname, Xnumber, Xpresent); XsetPokemons(""); XsetLastname(""); XsetNumber(""); XsetPresent(true)}}> <span class="text2">back</span> </button>   
-   <button className="square shape" onClick={e => {setSida(false); props.removePokemon(i); XsetPokemons(""); XsetLastname(""); XsetNumber(""); XsetPresent(true)}}><span class="text2">del</span></button>          
+   <button className="square shape" onClick={e => {setSida(false); props.updateUser(i, Xpokemons, Xlastname, Xnumber, Xpresent); XsetPokemons(""); XsetLastname(""); XsetNumber(""); XsetPresent(true)}}> <span className="text2">back</span> </button>   
+   <button className="square shape" onClick={e => {setSida(false); props.removePokemon(i); XsetPokemons(""); XsetLastname(""); XsetNumber(""); XsetPresent(true)}}><span className="text2">del</span></button>          
    </div>   
 }
 
